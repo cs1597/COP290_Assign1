ef1d0aa8799d9318b90a33517b744dd955bba389
Vishakha Agarwal <cs1221097@iitd.ac.in>
2024-01-31 00:53:26 +0530

    Added the user updation page

--- app.py
+++ app.py
@@ -14,6 +14,16 @@ import os
 import sys
 import plotly.express as px
 import plotly.graph_objects as go
+from flask_migrate import Migrate
+
+colors = [
+                    'blue', 'red', 'green', 'purple', 'orange', 'brown', 'cyan', 'magenta', 'yellow', 'black',
+                    'gray', 'pink', 'lightblue', 'lightcoral', 'lightgreen', 'lightgray', 'lightpink', 'lightyellow', 'darkblue', 'darkred',
+                    'darkgreen', 'darkpurple', 'darkorange', 'darkbrown', 'darkcyan', 'darkmagenta', 'darkyellow', 'darkgray', 'lightcyan',
+                    'lightmagenta', 'lightyellow', 'darkcyan', 'darkmagenta', 'darkyellow', 'lightcyan', 'lightmagenta', 'lightyellow',
+                    'darkcyan', 'darkmagenta', 'darkyellow', 'lightcyan', 'lightmagenta', 'lightyellow', 'darkcyan', 'darkmagenta', 'darkyellow'
+                ]
+df_nifty50=pd.read_csv('ind_nifty50list.csv')
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with your actual secret key
@@ -28,11 +38,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
+    full_name= db.Column(db.String(100), nullable=True)
+    mobile_number=db.Column(db.String(10),nullable=True)
+    email=db.Column(db.String(100),nullable=True)
 
 # Initialize Database within Application Context
 with app.app_context():
     db.create_all()
 
+migrate = Migrate(app,db)
 @app.route('/')
 def index():
     return render_template('login.html')
@@ -77,20 +91,49 @@ def dashboard():
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html', username=session['username'])
-
-@app.route('/user_info')
-def user_info():
-    return render_template('user_page.html',username=session['username'])
-
-
+    
 @app.route('/analyze_nifty', methods=['GET','POST'])
 def analyze_nifty():
     if request.method=="POST":
+        time_period = request.form['time_period']
+        end_date = datetime.now().date()
+        if time_period == '1W':
+            start_date = end_date - timedelta(weeks=1)
+        elif time_period == '1M':
+            start_date = end_date - timedelta(weeks=4)
+        elif time_period == '1Y':
+            start_date = end_date - timedelta(weeks=52)
+        elif time_period == '3Y':
+            start_date = end_date - timedelta(weeks=3*52)
+        elif time_period == '5Y':
+            start_date = end_date - timedelta(weeks=5*52)
+        else:
+            start_date = end_date - timedelta(weeks=2*52)
+        param=request.form["parameter"]
+        df = index_df(symbol="NIFTY 50", from_date=start_date, to_date=end_date)
+        df.sort_values(by=['HistoricalDate'],inplace=True)
+        trace1=go.Scatter(x=df['HistoricalDate'], y=df[param], mode='lines', name="NIFTY 50", line=dict(color='blue'))
+        layout=go.Layout(
+            title=f'Stock Prices for NIFTY50',
+            xaxis_title='Date',
+            yaxis_title=param,
+            legend=dict(x=0, y=1, traceorder='normal'),
+            xaxis=dict(
+                type='date',  
+                tickformat='%Y-%m-%d', 
+            ),
+            width=1500,
+            height=550
+        )
+        fig=go.Figure(data=trace1,layout=layout)
+        plot_html=fig.to_html(full_html=False)
+        return render_template('analyze_nifty.html',plot_html=plot_html)
+    else:
         end_date = datetime.now().date()
         start_date = end_date - timedelta(days=365 * 2)
-        df1 = index_df(symbol="NIFTY 50", from_date=start_date, to_date=end_date)
-        df1.sort_values(by=['HistoricalDate'],inplace=True)
-        trace1=go.Scatter(x=df1['HistoricalDate'], y=df1['CLOSE'], mode='lines', name="NIFTY 50", line=dict(color='blue'))
+        df = index_df(symbol="NIFTY 50", from_date=start_date, to_date=end_date)
+        df.sort_values(by=['HistoricalDate'],inplace=True)
+        trace1=go.Scatter(x=df['HistoricalDate'], y=df['CLOSE'], mode='lines', name="NIFTY 50", line=dict(color='blue'))
         layout=go.Layout(
                 title=f'Index price for NIFTY 50',
                 xaxis_title='Date',
@@ -100,8 +143,8 @@ def analyze_nifty():
                     type='date',  
                     tickformat='%Y-%m-%d', 
                 ),
-                width=800,
-                height=400
+                width=1500,
+                height=550
             )
         fig=go.Figure(data=trace1,layout=layout)
         plot_html=fig.to_html(full_html=False)
@@ -139,7 +182,7 @@ def stock_graph():
                 tickformat='%Y-%m-%d', 
             ),
             width=1500,
-            height=400
+            height=550
         )
         fig=go.Figure(data=trace1,layout=layout)
         plot_html=fig.to_html(full_html=False)
@@ -161,7 +204,7 @@ def stock_graph():
                 tickformat='%Y-%m-%d', 
             ),
             width=1500,
-            height=400
+            height=550
         )
         fig=go.Figure(data=trace1,layout=layout)
         plot_html=fig.to_html(full_html=False)
@@ -189,10 +232,11 @@ def compare():
             start_date = end_date - timedelta(weeks=2*52)
         plots = []
         param=request.form["parameter"]
-        for stck in symbols:
-            stck=stck.strip()
+
+        for i, stck in enumerate(symbols):
+            stck = stck.strip()
             df = stock_df(symbol=stck, from_date=start_date, to_date=end_date, series="EQ")
-            trace = go.Scatter(x=df['DATE'], y=df[param], mode='lines', name=stck, line=dict(color='blue'))
+            trace = go.Scatter(x=df['DATE'], y=df[param], mode='lines', name=stck, line=dict(color=colors[i]))
             plots.append(trace)
 
         layout = go.Layout(
@@ -205,7 +249,7 @@ def compare():
                 tickformat='%Y-%m-%d',
             ),
             width=1500,
-            height=400
+            height=550
         )
 
         fig = go.Figure(data=plots, layout=layout)
@@ -228,68 +272,99 @@ def compare():
                 tickformat='%Y-%m-%d', 
             ),
             width=1500,
-            height=400
+            height=550
         )
         fig=go.Figure(data=trace1,layout=layout)
         plot_html=fig.to_html(full_html=False)
         return render_template('compare.html')
-
-# @app.route('/multiple_stock_graphs', methods=['GET', 'POST'])
-# def multiple_stock_graphs():
-#     if request.method == 'POST':
-#         end_date = datetime.now().date()
-#         if 'stock1' in request.form:
-#             stck1=request.form['stock1']
-#             start_date = end_date - timedelta(weeks=2*52)
-#             session['selected_stock1'] = stck1 
-#         if 'stock2' in request.form:
-#             stck2=request.form['stock2']
-#             start_date = end_date - timedelta(weeks=2*52)
-#             session['selected_stock2'] = stck2 
-#         if 'time_period' in request.form:
-#             stck1=session.get('selected_stock1', None)
-#             stck2=session.get('selected_stock2', None) 
-#             time_period = request.form['time_period']
-#             end_date = datetime.now().date()
-#             if time_period == '1W':
-#                 start_date = end_date - timedelta(weeks=1)
-#             elif time_period == '1M':
-#                 start_date = end_date - timedelta(weeks=4)
-#             elif time_period == '1Y':
-#                 start_date = end_date - timedelta(weeks=52)
-#             elif time_period == '3Y':
-#                 start_date = end_date - timedelta(weeks=3*52)
-#             elif time_period == '5Y':
-#                 start_date = end_date - timedelta(weeks=5*52)
-#             else:
-#                 start_date = end_date - timedelta(weeks=2*52)
-#         df1 = stock_df(symbol=stck1, from_date=start_date, to_date=end_date, series="EQ")
-#         df2 = stock_df(symbol=stck2, from_date=start_date, to_date=end_date, series="EQ")
-#         trace1=go.Scatter(x=df1['DATE'], y=df1['CLOSE'], mode='lines', name=stck1, line=dict(color='blue'))
-#         trace2=go.Scatter(x=df2['DATE'], y=df2['CLOSE'], mode='lines', name=stck2, line=dict(color='red'))
-#         layout=go.Layout(
-#             title=f'Stock Prices for {stck1} and {stck2}',
-#             xaxis_title='Date',
-#             yaxis_title='Close Price',
-#             legend=dict(x=0, y=1, traceorder='normal'),
-#             xaxis=dict(
-#                 type='date',  
-#                 tickformat='%Y-%m-%d', 
-#             ),
-#             width=1500,
-#             height=400
-#         )
-#         fig=go.Figure(data=[trace1,trace2],layout=layout)
-#         plot_html=fig.to_html(full_html=False)
-#         return render_template('compare.html',plot_html=plot_html)
-#     else:
-#         return render_template('multiple_stock_form.html')                                                                                                                                                                                                                                       
-
+    
+@app.route('/filter_stocks', methods=['GET', 'POST'])
+def filter_stocks():
+    if request.method=="POST":
+        plots=[]
+        lower_bound=request.form["close_lower_bound"]
+        upper_bound=request.form["close_upper_bound"]
+        if lower_bound:
+            lower_bound=int(lower_bound)
+            upper_bound=int(upper_bound)
+            end_date = datetime.now().date()
+            start_date = end_date - timedelta(weeks=1)
+            filtered_list={}
+            for symbol in df_nifty50['Symbol']:
+                df_stock = stock_df(symbol=symbol, from_date=start_date, to_date=end_date, series="EQ")
+                if df_stock['CLOSE'].iloc[0]>=lower_bound and df_stock['CLOSE'].iloc[0]<=upper_bound:
+                    filtered_list[symbol]=df_stock
+            fig = go.Figure()
+            for symbol, filtered_df in filtered_list.items():
+                trace=(go.Bar(
+                    x=[symbol], 
+                    y=[filtered_df['CLOSE'].iloc[0]],  
+                    name=symbol,
+                    text=f"{symbol}: {filtered_df['CLOSE'].iloc[0]}", 
+                ))
+                plots.append(trace)
+            layout=go.Layout(
+                title='Close Prices for Filtered Symbols',
+                xaxis_title='Symbol',
+                yaxis_title='Close Price',
+                barmode='group', 
+                showlegend=True,
+                width=1500,
+                height=550
+            )
+        else:
+            lower_bound=int(request.form["value_lower_bound"])
+            upper_bound=int(request.form["value_upper_bound"])
+            end_date = datetime.now().date()
+            start_date = end_date - timedelta(weeks=1)
+            filtered_list={}
+            for symbol in df_nifty50['Symbol']:
+                df_stock = stock_df(symbol=symbol, from_date=start_date, to_date=end_date, series="EQ")
+                if df_stock['VALUE'].iloc[0]>=lower_bound and df_stock['VALUE'].iloc[0]<=upper_bound:
+                    filtered_list[symbol]=df_stock
+            fig = go.Figure()
+            for symbol, filtered_df in filtered_list.items():
+                trace=(go.Bar(
+                    x=[symbol], 
+                    y=[filtered_df['VALUE'].iloc[0]],  
+                    name=symbol,
+                    text=f"{symbol}: {filtered_df['VALUE'].iloc[0]}", 
+                ))
+                plots.append(trace)
+            layout=go.Layout(
+                title='Close Prices for Filtered Symbols',
+                xaxis_title='Symbol',
+                yaxis_title='Close Price',
+                barmode='group', 
+                showlegend=True,
+                width=1500,
+                height=550
+            )
+        fig = go.Figure(data=plots, layout=layout)
+        plot_html=fig.to_html(full_html=False)
+        return render_template('filter_stocks.html',plot_html=plot_html)
+    else:
+        return render_template('filter_stocks.html')
+ 
+    
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     session.pop('username', None)
     return redirect(url_for('index'))
+
+
+@app.route('/user_info')
+def user_info():
+    return render_template('user_info.html', username=session['username'])
+
+@app.route('/change_user_info')
+def change_user_info():
+    return render_template('change_user_info.html',username=session['username'])
+
+@app.route('/change_password')
+def change_password():
+    return render_template('change_password.html',username=session['username'])
 
 if __name__ == '__main__':
     app.run(debug=True)
